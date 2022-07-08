3f3d5fa53a720511c54b19b7dbbd0e1decb9ab6b
Pavel <pavel.818@yandex.ru>
2022-07-08 16:43:22 +0400

    Initial Commit

---
+++ MVC-TableView-iOS-5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-TableView-iOS-5
+//
+//  Created by Pavel Абрамов on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
